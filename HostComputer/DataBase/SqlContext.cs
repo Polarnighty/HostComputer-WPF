6e915dbceb91d3a4162839fd81bafb104fca8b87
Polarnighty <1041465280@qq.com>
2022-03-03 22:49:25 +0800

    完成后端数据库架构

---
+++ HostComputer/DataBase/SqlContext.cs
@@ -0,0 +1,20 @@
+﻿using HostComputer.Entity;
+using Microsoft.EntityFrameworkCore;
+using System.Configuration;
+
+namespace HostComputer.DataBase
+{
+    public class SqlContext:DbContext
+    {
+        public DbSet<UserInfo> userInfos { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(ConfigurationManager.ConnectionStrings["BloggingDatabase"].ConnectionString);
+        }
+
+    }
+}
